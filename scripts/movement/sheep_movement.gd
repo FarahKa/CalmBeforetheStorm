1cdaa69ddbef5de469c183343994338e489f14f1
Farah <kacem.farah@insat.u-carthage.tn>
2024-09-08 17:40:18 +0200

    sheep and placeholder map

---
+++ scripts/movement/sheep_movement.gd
@@ -0,0 +1,59 @@
+extends CharacterBody2D
+
+var start_position
+var target_position 
+
+func _ready() -> void:
+	start_position = global_position
+	target_position = global_position
+	velocity = Vector2.ZERO
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+enum {
+	IDLE,
+	WANDER
+}
+
+
+var state = IDLE
+
+const ACCELERATION = 300
+const MAX_SPEED = 50
+const TOLERANCE = 4.0
+
+
+
+func update_target_position():
+	var target_vector = Vector2(randf_range(-32, 32), randf_range(-32, 32))
+	target_position = start_position + target_vector
+
+func is_at_target_position(): 
+	# Stop moving when at target +/- tolerance
+	return (target_position - global_position).length() < TOLERANCE
+
+func _physics_process(delta):
+	match state:
+		IDLE:
+			state = WANDER
+			# Maybe wait for X seconds with a timer before moving on
+			update_target_position()
+		WANDER:
+			accelerate_to_point(target_position, ACCELERATION * delta)
+			if is_at_target_position():
+				state = IDLE
+
+	move_and_slide()
+
+func accelerate_to_point(point, acceleration_scalar):
+	var direction = (point - global_position).normalized()
+	var acceleration_vector = direction * acceleration_scalar
+	accelerate(acceleration_vector)
+
+func accelerate(acceleration_vector):
+	velocity += acceleration_vector
+	velocity = velocity.limit_length(MAX_SPEED)
