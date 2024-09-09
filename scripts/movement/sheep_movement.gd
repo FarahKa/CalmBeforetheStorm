382c5d247df12b735a89caab8436652f16116367
Farah <kacem.farah@insat.u-carthage.tn>
2024-09-09 20:41:56 +0200

    made sheep run away from dog and made many sheep. chenille problem to fix

--- scripts/movement/sheep_movement.gd
+++ scripts/movement/sheep_movement.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 var start_position
 var target_position 
+@onready var dog: CharacterBody2D = $"../Dog"
 
 func _ready() -> void:
 	start_position = global_position
@@ -25,6 +26,7 @@ var state = IDLE
 const ACCELERATION = 300
 const MAX_SPEED = 50
 const TOLERANCE = 4.0
+const SPEED_AWAY =  100
 
 
 
@@ -44,6 +46,17 @@ func _physics_process(delta):
 			update_target_position()
 		WANDER:
 			accelerate_to_point(target_position, ACCELERATION * delta)
+			#print(global_position.distance_to(dog.global_position))
+			#print(velocity)
+			if velocity == Vector2.ZERO:
+				update_target_position()
+				velocity=Vector2(20,20)
+				
+			if global_position.distance_to(dog.global_position) < 100:
+				var direction = global_position.direction_to(dog.global_position)
+				velocity = -direction * SPEED_AWAY
+				
+			
 			if is_at_target_position():
 				state = IDLE
 
