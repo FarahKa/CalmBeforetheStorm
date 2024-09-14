9f4779374a9da2ed466c8f5611885a3f1177951c
Victor Meunier <UristMacGitHub>
2024-09-14 23:43:23 +0200

    le chien aboie

--- scripts/movement/player.gd
+++ scripts/movement/player.gd
@@ -7,6 +7,8 @@ extends CharacterBody2D
 var input_direction
 var direction
 var input_action
+var bark_noise
+var number_barked
 	
 func _physics_process(_delta):
 	get_input()
@@ -18,6 +20,7 @@ func _physics_process(_delta):
 		$AnimatedSprite2D.flip_h = true
 	if input_action :
 		var is_barking = "true"
+		$AudioStreamPlayer.play()
 		if direction == 0 :
 			$AnimatedSprite2D.play("idle_bark")
 		elif direction != 0 :
