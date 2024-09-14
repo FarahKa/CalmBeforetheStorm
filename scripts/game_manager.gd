e491e5bb76b19f08e8984d124fa96e4cf8af596e
Farah <kacem.farah@insat.u-carthage.tn>
2024-09-15 00:34:43 +0200

    gameplay loop done ish?

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,6 +2,8 @@ extends Node
 @onready var dead: Label = $Dead
 
 @onready var gameover: Label = $GAMEOVER
+@onready var timer: Timer = $Timer
+@onready var time_left: Label = $TimeLeft
 
 var dead_nb = 0
 var max
@@ -10,6 +12,18 @@ var max
 func _ready() -> void:
 	max = sheepz.get_child_count()
 	
+	
+	
+func _process(delta: float) -> void:
+	print(timer.time_left)
+	time_left.text=(str(int(timer.time_left)) + " SECONDS BEFORE THE STORM")
+	if timer.time_left == 5:
+		Engine.time_scale = 0.5
+	if timer.time_left == 0:
+		Engine.time_scale = 0
+		gameover.text = "STORM IS HERE.\n" + str(max-dead_nb) + " SHEEP LEFT."
+		gameover.visible = true
+
 func add_death():
 	dead_nb += 1
 	print(dead_nb)
